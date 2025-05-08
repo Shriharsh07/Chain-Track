7880e534f12e75e34f6acd201bdc0858c3432b98
Shriharsh07 <shriharshdpattar@gmail.com>
2025-05-08 22:42:22 +0530

    Updated the db connection string

--- config/db.go
+++ config/db.go
@@ -25,7 +25,7 @@ func ConnectDB() error {
 
 	// Build connection string
 	dsn := fmt.Sprintf(
-		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		"%s:%s@tcp(%s:%s)/%s",
 		db_user,
 		db_pass,
 		db_host,
