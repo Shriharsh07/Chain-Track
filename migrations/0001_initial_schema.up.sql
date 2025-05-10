cd1b22ac9d167afadf89cbc453e5e0009ebff782
Shriharsh07 <shriharshdpattar@gmail.com>
2025-05-10 18:53:30 +0530

    Updated the transaction table to include block id

--- migrations/0001_initial_schema.up.sql
+++ migrations/0001_initial_schema.up.sql
@@ -4,6 +4,7 @@ CREATE TABLE `transactions` (
   `receiver` longtext,
   `amount` double DEFAULT NULL,
   `is_mined` tinyint(1) DEFAULT '0',
+  `block_id` bigint unsigned DEFAULT NULL,
   `created_at` datetime(3) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
